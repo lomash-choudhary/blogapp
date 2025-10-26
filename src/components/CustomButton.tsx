071380ec8a495d81f00b2a0d55564098ecec9aca
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-10-26 22:15:50 +0530

    added custom button and modified the header according to the user state with some nav items

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,27 @@
+import type React from "react";
+
+const CustomButton = ({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}: {
+  children: React.ReactNode;
+  type: string;
+  bgColor: string;
+  textColor: string;
+  className: string;
+}) => {
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg ${bgColor}, ${textColor}, ${className}, ${type}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CustomButton;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,82 @@
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+import { useSelector } from "react-redux";
+import type { RootState } from "../../store/store";
+import { Link, useNavigate } from "react-router-dom";
+import Container from "../container/Container";
+import Logo from "../Logo";
+import LogoutBtn from "../LogoutBtn";
 
-export default Header
+const Header = () => {
+  // useSelector accepts a call back in that callback we get state.
+  // we can access values from our state like this
+  // we will use state, then we will use a dot operator and have the name of the slice and from there we can access the status and userData as well
+  const userAuthStatus = useSelector(
+    (state: RootState) => state.AuthSlice.status
+  );
+  const navigate = useNavigate();
+
+  // when we have to create a navigation bar then we have to make an array and then loop over that array
+
+  const navItems = [
+    {
+      name: "Home",
+      url: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      url: "/login",
+      active: !userAuthStatus,
+    },
+    {
+      name: "Signup",
+      url: "/signup",
+      active: !userAuthStatus,
+    },
+    {
+      name: "All Posts",
+      url: "/all-posts",
+      active: userAuthStatus,
+    },
+    {
+      name: "Add Post",
+      url: "/add-post",
+      active: userAuthStatus,
+    },
+  ];
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to={"/"}>
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.url)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {
+                userAuthStatus && (
+                    <li>
+                        <LogoutBtn />
+                    </li>
+                )
+            }
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
