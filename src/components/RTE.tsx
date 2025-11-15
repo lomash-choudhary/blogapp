ba43e468505ba2398216fc0832230fbb4ddbfcbb
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-11-15 18:45:14 +0530

    added Real Time Editor

---
+++ src/components/RTE.tsx
@@ -0,0 +1,64 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+const RTE = ({
+  name,
+  control,
+  label,
+  defaultValue = "",
+}: {
+  name: string;
+  control: any;
+  label: string;
+  defaultValue: string;
+}) => {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        // this means that if anything changes on this field then inform me about that with a render
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+            // if anything is changing in editor then our fields are getting governed by onchange
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default RTE;
